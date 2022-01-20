58366b9af7b6633c91ac65aefb1e7a52cc8c828c
903345072 <903345072@qq.com>
2022-01-20 13:31:22 +0800

    ads

--- lib/net/Address.dart
+++ lib/net/Address.dart
@@ -1,7 +1,7 @@
 class Address{
   // static String BASE_URL = "http://192.168.1.4:86/";  //192.168.31.199
-   static String BASE_URL = "http://hlitongccc.com:81/";  //192.168.31.199
+   static String BASE_URL = "http://8.134.64.96:81/";  //192.168.31.199
   //static String WEBSOCKET_GET_ORDER_URL = 'ws://192.168.1.4:1234/ws?path=getOrder&hid=';  //192.168.31.199
-  static String WEBSOCKET_GET_ORDER_URL = 'ws://hlitongccc.com:81/ws?path=getOrder&hid=';  //192.168.31.199
+  static String WEBSOCKET_GET_ORDER_URL = 'ws://8.134.64.96:81/ws?path=getOrder&hid=';  //192.168.31.199
   static String TEST_API ="/v2/movie/in_theaters";
 }
